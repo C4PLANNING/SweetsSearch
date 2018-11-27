8156c6515b82dfe4a88813591201d4e1e9546a0c
aweshin <aweshin.android@gmail.com>
2018-11-27 12:28:25 +0900

    edit README.md

--- mobile/src/main/java/io/shin/sweetssearch/MainActivity.java
+++ mobile/src/main/java/io/shin/sweetssearch/MainActivity.java
@@ -2,9 +2,12 @@ package io.shin.sweetssearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new ButtonOnClickListener());
     }
 
     @Override
@@ -33,5 +39,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ButtonOnClickListener implements OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            // 引数1：自身のActivity、引数2:移動先のActivity名
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            // Activityの移動
+            startActivity(intent);
+        }
+
     }
 }
